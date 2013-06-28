a906f919b6a3b3c40ecde5109a478895a676d107
Ted Gould <ted@gould.cx>
2013-06-28 09:37:21 -0500

    A little command line utility using the lib

---
+++ tools/url-dispatcher.c
@@ -0,0 +1,33 @@
+
+#include "url-dispatch.h"
+
+static gboolean global_succcess = FALSE;
+
+static void
+callback (const gchar * url, gboolean success, gpointer user_data)
+{
+	global_success = success;
+	g_main_loop_quit((GMainLoop *)user_data);
+	return;
+}
+
+int
+main (int argc, char * argv[])
+{
+	if (argc != 2) {
+		g_printerr("Usage: %s <url>\n", argv[0]);
+		return 1;
+	}
+
+	GMainLoop * mainloop = g_main_loop_new(NULL, FALSE);
+
+	url_dispatch_send(argv[1], callback, mainloop);
+
+	g_main_loop_run(mainloop);
+
+	if (global_success) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
