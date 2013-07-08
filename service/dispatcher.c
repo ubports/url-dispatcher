44bf76987f2e029f2df1fed3f5765d67db00527d
Ted Gould <ted@gould.cx>
2013-07-08 10:24:07 -0500

    Got a main loop, let's get started

--- service/dispatcher.c
+++ service/dispatcher.c
@@ -1,9 +1,19 @@
+#include <gio/gio.h>
+
+/* Globals */
+GMainLoop * mainloop = NULL;
 
 int
 main (int argc, char * argv[])
 {
+	mainloop = g_main_loop_new(NULL, FALSE);
 
 
+	/* Run Main */
+	g_main_loop_run(mainloop);
+
+	/* Clean up globals */
+	g_main_loop_unref(mainloop);
 
 	return 0;
 }
