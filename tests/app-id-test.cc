27583e630acb8f2ec7b73ee8ecc768240487c724
Ted Gould <ted@gould.cx>
2013-09-26 14:56:16 -0500

    Adding in an app id test

--- tests/app-id-test.cc
+++ tests/app-id-test.cc
@@ -1,15 +1,62 @@
+/**
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+ * SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
+
+#include <gio/gio.h>
 #include <gtest/gtest.h>
+#include "dispatcher.h"
+#include "upstart-app-launch-mock.h"
 
 class AppIdTest : public ::testing::Test
 {
+	private:
+		GTestDBus * testbus = NULL;
+		GMainLoop * mainloop = NULL;
+
 	protected:
 		virtual void SetUp() {
+			testbus = g_test_dbus_new(G_TEST_DBUS_NONE);
+			g_test_dbus_up(testbus);
+
+			mainloop = g_main_loop_new(NULL, FALSE);
+			dispatcher_init(mainloop);
+
+			return;
+		}
+
+		virtual void TearDown() {
+			dispatcher_shutdown();
+
+			/* Clean up queued events */
+			while (g_main_pending()) {
+				g_main_iteration(TRUE);
+			}
+
+			g_main_loop_unref(mainloop);
+
+			upstart_app_launch_mock_clear_last_app_id();
+
+			g_test_dbus_down(testbus);
+			g_object_unref(testbus);
 			return;
 		}
 };
 
 TEST_F(AppIdTest, StubTest)
 {
-	return;
+	ASSERT_TRUE(TRUE);
 }
