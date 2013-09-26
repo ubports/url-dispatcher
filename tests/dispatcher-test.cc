3e584255561341fade07d31e5c101a6942a21044
Ted Gould <ted@gould.cx>
2013-09-26 10:07:09 -0500

    Adding a stub test in

---
+++ tests/dispatcher-test.cc
@@ -0,0 +1,17 @@
+
+#include <gtest/gtest.h>
+
+class DispatcherTest : public ::testing::Test
+{
+	private:
+
+	protected:
+		virtual void SetUp() {
+			return;
+		}
+};
+
+TEST_F(DispatcherTest, StubTest)
+{
+	return;
+}
