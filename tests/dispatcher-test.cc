13f908cf6c13162195c3b2c3bfc81b2a98170673
Ted Gould <ted@gould.cx>
2013-09-26 10:35:56 -0500

    Set up the dispatcher and a bus to run it under

--- tests/dispatcher-test.cc
+++ tests/dispatcher-test.cc
@@ -1,13 +1,37 @@
 
+#include <gio/gio.h>
 #include <gtest/gtest.h>
 #include "dispatcher.h"
 
 class DispatcherTest : public ::testing::Test
 {
 	private:
+		GTestDBus * testbus = NULL;
+		GMainLoop * mainloop = NULL;
 
 	protected:
 		virtual void SetUp() {
+			testbus = g_test_dbus_new(G_TEST_DBUS_NONE);
+			g_test_dbus_up(testbus);
+
+			mainloop = g_main_loop_new(NULL, FALSE);
+			dispatcher_init(mainloop);
+
+			return;
+		}
+
+		virtual void TearDown() {
+			dispatcher_shutdown();
+
+			/* Clean up queued events */
+			while (g_main_pending()) {
+				g_main_iteration(TRUE);
+			}
+
+			g_main_loop_unref(mainloop);
+
+			g_test_dbus_down(testbus);
+			g_object_unref(testbus);
 			return;
 		}
 };
