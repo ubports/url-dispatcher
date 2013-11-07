e5bbc8269020f94f154aeed5299c865dbb85d581
Ted Gould <ted@gould.cx>
2013-11-07 09:12:37 -0600

    Setup our fixture

--- tests/lib-test.cc
+++ tests/lib-test.cc
@@ -24,11 +24,46 @@
 class LibTest : public ::testing::Test
 {
 	protected:
+		DbusTestService * service = NULL;
+		DbusTestDbusMock * mock = NULL;
+		DbusTestDbusMockObject * obj = NULL;
+		GDBusConnection * bus = NULL;
+
 		virtual void SetUp() {
+			service = dbus_test_service_new(NULL);
+
+			mock = dbus_test_dbus_mock_new("com.canonical.URLDispatcher");
+			obj = dbus_test_dbus_mock_get_object(mock, "/com/canonical/URLDispatcher", "com.canonical.URLDispatcher", NULL);
+
+			dbus_test_dbus_mock_object_add_method(mock, obj,
+				"DispatchURL",
+				G_VARIANT_TYPE_STRING,
+				NULL, /* out */
+				"", /* python */
+				NULL); /* error */
+
+			dbus_test_service_add_task(service, DBUS_TEST_TASK(mock));
+			dbus_test_service_start_tasks(service);
+
+			bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+			g_dbus_connection_set_exit_on_close(bus, FALSE);
+			g_object_add_weak_pointer(G_OBJECT(bus), (gpointer *)&bus);
 			return;
 		}
 
 		virtual void TearDown() {
+			g_clear_object(&mock);
+			g_clear_object(&service);
+
+			g_object_unref(bus);
+
+			unsigned int cleartry = 0;
+			while (bus != NULL && cleartry < 100) {
+				g_usleep(100000);
+				while (g_main_pending())
+					g_main_iteration(TRUE);
+				cleartry++;
+			}
 			return;
 		}
 };
