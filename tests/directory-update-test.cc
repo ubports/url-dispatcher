1f89a0751736c3f3e2f085d038158c947b552392
Ted Gould <ted@gould.cx>
2014-01-10 11:02:05 -0600

    Adding a test for a simple single entry that is good

--- tests/directory-update-test.cc
+++ tests/directory-update-test.cc
@@ -102,3 +102,16 @@ TEST_F(DirectoryUpdateTest, DirDoesntExist)
 
 	sqlite3_close(db);
 };
+
+TEST_F(DirectoryUpdateTest, SingleGoodItem)
+{
+	gchar * cmdline = g_strdup_printf("%s \"%s\"", UPDATE_DIRECTORY_TOOL, UPDATE_DIRECTORY_URLS);
+	g_spawn_command_line_sync(cmdline, NULL, NULL, NULL, NULL);
+	g_free(cmdline);
+
+	sqlite3 * db = url_db_create_database();
+	EXPECT_EQ(1, get_file_count(db));
+	EXPECT_EQ(1, get_url_count(db));
+
+	sqlite3_close(db);
+};
