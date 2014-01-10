7293513344b4438c5c8fa05f9173ff62d7c0e75e
Ted Gould <ted@gould.cx>
2014-01-10 10:40:27 -0600

    Adding a basic test for the directory update tool

---
+++ tests/directory-update-test.cc
@@ -0,0 +1,44 @@
+/**
+ * Copyright © 2014 Canonical, Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranties of MERCHANTABILITY,
+ * SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <glib.h>
+
+class DirectoryUpdateTest : public ::testing::Test
+{
+	protected:
+		gchar * cachedir = NULL;
+
+		virtual void SetUp() {
+			cachedir = g_build_filename(CMAKE_BINARY_DIR, "url-db-test-cache", NULL);
+			g_setenv("URL_DISPATCHER_CACHE_DIR", cachedir, TRUE);
+		}
+
+		virtual void TearDown() {
+			gchar * cmdline = g_strdup_printf("rm -rf \"%s\"", cachedir);
+			g_spawn_command_line_sync(cmdline, NULL, NULL, NULL, NULL);
+			g_free(cmdline);
+			g_free(cachedir);
+		}
+};
+
+TEST_F(DirectoryUpdateTest, DirDoesntExist)
+{
+
+
+
+};
