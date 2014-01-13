dc48783d2c114a4ceade66777144da6f24eaf339
Ted Gould <ted@gould.cx>
2014-01-13 11:27:34 -0600

    Test to ensure removing a file removes the entiry in the DB

--- tests/directory-update-test.cc
+++ tests/directory-update-test.cc
@@ -239,3 +239,46 @@ TEST_F(DirectoryUpdateTest, VariedItems)
 	sqlite3_close(db);
 };
 
+TEST_F(DirectoryUpdateTest, RemoveFile)
+{
+	gchar * cmdline;
+	sqlite3 * db = url_db_create_database();
+
+	/* A temporary directory to put files in */
+	gchar * datadir = g_build_filename(CMAKE_BINARY_DIR, "remove-file-data", NULL);
+	g_mkdir_with_parents(datadir,  1 << 6 | 1 << 7 | 1 << 8); // 700
+	ASSERT_TRUE(g_file_test(datadir, (GFileTest)(G_FILE_TEST_EXISTS | G_FILE_TEST_IS_DIR)));
+
+	/* Copy the files */
+	cmdline = g_strdup_printf("cp \"%s/%s\" \"%s\"", UPDATE_DIRECTORY_URLS, "single-good.url-dispatcher", datadir);
+	g_spawn_command_line_sync(cmdline, NULL, NULL, NULL, NULL);
+	g_free(cmdline);
+
+	/* Run the tool */
+	cmdline = g_strdup_printf("%s \"%s\"", UPDATE_DIRECTORY_TOOL, datadir);
+	g_spawn_command_line_sync(cmdline, NULL, NULL, NULL, NULL);
+	g_free(cmdline);
+
+	EXPECT_EQ(1, get_file_count(db));
+	EXPECT_EQ(1, get_url_count(db));
+
+	/* Kill the files */
+	cmdline = g_strdup_printf("rm \"%s/%s\"", datadir, "single-good.url-dispatcher");
+	g_spawn_command_line_sync(cmdline, NULL, NULL, NULL, NULL);
+	g_free(cmdline);
+
+	/* Run the tool */
+	cmdline = g_strdup_printf("%s \"%s\"", UPDATE_DIRECTORY_TOOL, datadir);
+	g_spawn_command_line_sync(cmdline, NULL, NULL, NULL, NULL);
+	g_free(cmdline);
+
+	EXPECT_EQ(0, get_file_count(db));
+	EXPECT_EQ(0, get_url_count(db));
+
+	/* Cleanup */
+	cmdline = g_strdup_printf("rm -rf \"%s\"", datadir);
+	g_spawn_command_line_sync(cmdline, NULL, NULL, NULL, NULL);
+	g_free(cmdline);
+
+	sqlite3_close(db);
+}
