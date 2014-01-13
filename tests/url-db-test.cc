8e6a954e53c7b15c8193f72fd0b935c8ab9e0b14
Ted Gould <ted@gould.cx>
2014-01-13 09:22:36 -0600

    Adding a test to find the files

--- tests/url-db-test.cc
+++ tests/url-db-test.cc
@@ -34,6 +34,23 @@ class UrlDBTest : public ::testing::Test
 			g_free(cmdline);
 			g_free(cachedir);
 		}
+
+		bool file_list_has (GList * list, const gchar * filename) {
+			GList * cur;
+
+			for (cur = list; cur != NULL; cur = g_list_next(cur)) {
+				const gchar * path = (const gchar *)cur->data;
+				gchar * basename = g_path_get_basename(path);
+				gint same = g_strcmp0(basename, filename);
+				g_free(basename);
+
+				if (same == 0) {
+					return true;
+				}
+			}
+
+			return false;
+		}
 };
 
 TEST_F(UrlDBTest, CreateTest) {
@@ -102,5 +119,41 @@ TEST_F(UrlDBTest, UrlTest) {
 	EXPECT_STREQ("bar", url_db_find_url(db, "bar", "more.foo.com"));
 	EXPECT_STREQ("bar", url_db_find_url(db, "bar", "www.more.foo.com"));
 
+	sqlite3_close(db);
+}
+
+TEST_F(UrlDBTest, FileListTest) {
+	sqlite3 * db = url_db_create_database();
+
+	ASSERT_TRUE(db != NULL);
+
+	GTimeVal timeval = {0};
+	timeval.tv_sec = 12345;
+
+	/* One Dir */
+	EXPECT_TRUE(url_db_set_file_motification_time(db, "/base/direcory/for/us/one.url-dispatcher", &timeval));
+	EXPECT_TRUE(url_db_set_file_motification_time(db, "/base/direcory/for/us/two.url-dispatcher", &timeval));
+	/* No three! */
+	EXPECT_TRUE(url_db_set_file_motification_time(db, "/base/direcory/for/us/four.url-dispatcher", &timeval));
+	EXPECT_TRUE(url_db_set_file_motification_time(db, "/base/direcory/for/us/five.url-dispatcher", &timeval));
+
+	/* Another Dir */
+	EXPECT_TRUE(url_db_set_file_motification_time(db, "/base/direcory/for/them/six.url-dispatcher", &timeval));
+
+	GList * files = url_db_files_for_dir(db, "/base/directory/for/us");
+	EXPECT_TRUE(file_list_has(files, "one.url-dispatcher"));
+	EXPECT_TRUE(file_list_has(files, "two.url-dispatcher"));
+	EXPECT_FALSE(file_list_has(files, "three.url-dispatcher"));
+	EXPECT_TRUE(file_list_has(files, "four.url-dispatcher"));
+	EXPECT_TRUE(file_list_has(files, "five.url-dispatcher"));
+	EXPECT_FALSE(file_list_has(files, "six.url-dispatcher"));
+
+	g_list_free_full(files, g_free);
+
+	files = url_db_files_for_dir(db, "/base/directory/for/them");
+	EXPECT_FALSE(file_list_has(files, "five.url-dispatcher"));
+	EXPECT_TRUE(file_list_has(files, "six.url-dispatcher"));
+
+	g_list_free_full(files, g_free);
 	sqlite3_close(db);
 }
