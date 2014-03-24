48121e744d21171ff6964ce4d0a0e5a090d15e6e
Ted Gould <ted@gould.cx>
2014-03-24 15:43:29 -0500

    Make these tests work with the new scheme better

--- tests/app-id-test.cc
+++ tests/app-id-test.cc
@@ -130,7 +130,7 @@ TEST_F(AppIdTest, OrderingUrl)
 
 TEST_F(AppIdTest, BadDirectory)
 {
-	g_setenv("URL_DISPATCHER_TEST_CLICK_DIR", CMAKE_SOURCE_DIR "/not-a-real-directory/", TRUE);
+	g_setenv("TEST_CLICK_DB", "not-click-db", TRUE);
 
 	dispatch_url("appid://com.test.good/app1/current-user-version");
 	ASSERT_TRUE(NULL == upstart_app_launch_mock_get_last_app_id());
@@ -139,9 +139,9 @@ TEST_F(AppIdTest, BadDirectory)
 	return;
 }
 
-TEST_F(AppIdTest, BadClick)
+TEST_F(AppIdTest, BadUser)
 {
-	g_unsetenv("URL_DISPATCHER_TEST_CLICK_DIR");
+	g_setenv("TEST_CLICK_USER", "not-test-user", TRUE);
 
 	dispatch_url("appid://com.test.good/app1/current-user-version");
 	ASSERT_TRUE(NULL == upstart_app_launch_mock_get_last_app_id());
