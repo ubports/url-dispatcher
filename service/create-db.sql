7d9ed32a5668877fbd0bc47caf4325a45b870223
Ted Gould <ted@gould.cx>
2014-07-07 14:58:38 -0500

    Put the database into WAL mode for shared memory overlaping of writes

--- service/create-db.sql
+++ service/create-db.sql
@@ -1,3 +1,4 @@
+pragma journal_mode = WAL;
 create table if not exists configfiles (name text unique, timestamp bigint);
 create table if not exists urls (sourcefile integer, protocol text, domainsuffix text);
 create unique index if not exists urls_index on urls (sourcefile, protocol, domainsuffix);
