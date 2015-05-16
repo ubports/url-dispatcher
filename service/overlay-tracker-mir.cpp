7eca6598a08c29fa021d78e3fd7fe12d4d8c7fde
Ted Gould <ted@gould.cx>
2015-05-15 22:10:42 -0500

    Setup a helper and start mir session tracking

--- service/overlay-tracker-mir.cpp
+++ service/overlay-tracker-mir.cpp
@@ -1,4 +1,7 @@
 #include "overlay-tracker-mir.h"
+#include <ubuntu-app-launch.h>
+
+static const char * HELPER_TYPE = "url-overlay";
 
 OverlayTrackerMir::OverlayTrackerMir (void) 
 	: thread([this] {
@@ -31,6 +34,40 @@ OverlayTrackerMir::addOverlay (const char * appid, unsigned long pid)
 
 	thread.executeOnThread([this, sappid, pid] {
 		g_debug("Setting up over lay for PID %d with '%s'", pid, sappid.c_str());
+
+		auto session = std::shared_ptr<MirPromptSession>(
+			mir_connection_create_prompt_session_sync(mir.get(), pid, sessionStateChangedStatic, this),
+			[] (MirPromptSession * session) { if (session) mir_prompt_session_release_sync(session); });
+		
+		auto instance = ubuntu_app_launch_start_session_helper(HELPER_TYPE, session.get(), sappid.c_str(), nullptr /* TODO */);
+
+		ongoingSessions.emplace(std::make_pair(std::string(instance), session));
 	});
 }
 
+void
+OverlayTrackerMir::sessionStateChangedStatic (MirPromptSession * session, MirPromptSessionState state, void * user_data)
+{
+	reinterpret_cast<OverlayTrackerMir *>(user_data)->sessionStateChanged(session, state);
+}
+
+void
+OverlayTrackerMir::sessionStateChanged (MirPromptSession * session, MirPromptSessionState state)
+{
+	if (state != mir_prompt_session_state_stopped) {
+		/* We only care about the stopped state */
+		return;
+	}
+
+	/* Executing on the Mir thread, which is nice and all, but we
+	   want to get back on our thread */
+	thread.executeOnThread([this, session]() {
+		for (auto it = ongoingSessions.begin(); it != ongoingSessions.end(); it++) {
+			if (it->second.get() == session) {
+				ubuntu_app_launch_stop_multiple_helper(HELPER_TYPE, "foo" /* TODO */, it->first.c_str());
+				ongoingSessions.erase(it);
+				break;
+			}
+		}
+	});
+}
--- service/overlay-tracker-mir.h
+++ service/overlay-tracker-mir.h
@@ -1,4 +1,8 @@
 
+#pragma once
+
+#include <set>
+
 #include <mir_toolkit/mir_connection.h>
 #include <mir_toolkit/mir_prompt_session.h>
 
@@ -9,8 +13,12 @@ class OverlayTrackerMir : public OverlayTrackerIface {
 private:
 	GLib::ContextThread thread;
 	std::shared_ptr<MirConnection> mir;
+	std::set<std::pair<std::string, std::shared_ptr<MirPromptSession>>> ongoingSessions;
 
 public:
 	OverlayTrackerMir (void); 
 	void addOverlay (const char * appid, unsigned long pid) override;
+
+	static void sessionStateChangedStatic (MirPromptSession * session, MirPromptSessionState state, void * user_data);
+	void sessionStateChanged (MirPromptSession * session, MirPromptSessionState state);
 };
