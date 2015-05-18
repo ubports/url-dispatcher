7e61e1c5c93bd9f93b65d01837f51c9a491b88b2
Ted Gould <ted@gould.cx>
2015-05-18 16:14:05 -0500

    Adding in a base for the overlay tracker testing

---
+++ tests/overlay-tracker-test.cpp
@@ -0,0 +1,90 @@
+
+#include "test-config.h"
+
+#include <gio/gio.h>
+#include <gtest/gtest.h>
+#include <libdbustest/dbus-test.h>
+
+#include "overlay-tracker-mir.h"
+
+class OverlayTrackerTest : public ::testing::Test
+{
+	protected:
+		DbusTestService * service = nullptr;
+		DbusTestDbusMock * mock = nullptr;
+		DbusTestDbusMockObject * obj = nullptr;
+		DbusTestDbusMockObject * jobobj = nullptr;
+		GDBusConnection * bus = nullptr;
+
+		virtual void SetUp() {
+			g_setenv("UBUNTU_APP_LAUNCH_USE_SESSION", "1", TRUE);
+			g_setenv("URL_DISPATCHER_DISABLE_RECOVERABLE_ERROR", "1", TRUE);
+
+			service = dbus_test_service_new(nullptr);
+
+			/* Upstart Mock */
+			mock = dbus_test_dbus_mock_new("com.ubuntu.Upstart");
+			obj = dbus_test_dbus_mock_get_object(mock, "/com/ubuntu/Upstart", "com.ubuntu.Upstart0_6", nullptr);
+
+			dbus_test_dbus_mock_object_add_method(mock, obj,
+				"GetJobByName",
+				G_VARIANT_TYPE_STRING,
+				G_VARIANT_TYPE_OBJECT_PATH, /* out */
+				"ret = dbus.ObjectPath('/job')", /* python */
+				nullptr); /* error */
+
+			jobobj = dbus_test_dbus_mock_get_object(mock, "/job", "com.ubuntu.Upstart0_6.Job", nullptr);
+
+			dbus_test_dbus_mock_object_add_method(mock, jobobj,
+				"Start",
+				G_VARIANT_TYPE("(asb)"),
+				G_VARIANT_TYPE_OBJECT_PATH, /* out */
+				"ret = dbus.ObjectPath('/instance')", /* python */
+				nullptr); /* error */
+
+			dbus_test_task_set_name(DBUS_TEST_TASK(mock), "Upstart");
+			dbus_test_service_add_task(service, DBUS_TEST_TASK(mock));
+
+			/* Start your engines! */
+			dbus_test_service_start_tasks(service);
+
+			bus = g_bus_get_sync(G_BUS_TYPE_SESSION, nullptr, nullptr);
+			g_dbus_connection_set_exit_on_close(bus, FALSE);
+			g_object_add_weak_pointer(G_OBJECT(bus), (gpointer *)&bus);
+			return;
+		}
+
+		virtual void TearDown() {
+			g_clear_object(&mock);
+			g_clear_object(&service);
+
+			g_object_unref(bus);
+
+			unsigned int cleartry = 0;
+			while (bus != nullptr && cleartry < 100) {
+				pause(100);
+				while (g_main_pending())
+					g_main_iteration(TRUE);
+				cleartry++;
+			}
+
+			return;
+		}
+
+		static gboolean quit_loop (gpointer ploop) {
+			g_main_loop_quit((GMainLoop *)ploop);
+			return FALSE;
+		}
+
+		void pause (int time) {
+			GMainLoop * loop = g_main_loop_new(nullptr, FALSE);
+			g_timeout_add(time, quit_loop, loop);
+			g_main_loop_run(loop);
+			g_main_loop_unref(loop);
+		}
+};
+
+TEST_F(OverlayTrackerTest, BasicCreation) {
+	auto tracker = new OverlayTrackerMir();
+	delete tracker;
+}
