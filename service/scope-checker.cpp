678b46dd01f8b0c1008539be5ad6e23cbca6d99b
Ted Gould <ted@gould.cx>
2015-06-22 14:01:06 -0500

    Adding a PID check

--- service/scope-checker.cpp
+++ service/scope-checker.cpp
@@ -1,6 +1,8 @@
 
 extern "C" {
 #include "scope-checker.h"
+#include <sys/apparmor.h>
+#include <ubuntu-app-launch.h>
 }
 
 #include <unity-scopes.h>
@@ -35,4 +37,39 @@ scope_checker_is_scope (ScopeChecker * checker, const char * appid)
 	} catch (...) {
 		return 0;
 	}
+}
+
+int
+scope_checker_is_scope_pid (ScopeChecker * checker, pid_t pid)
+{
+	if (pid == 0)
+		return 0;
+
+	char * aa = nullptr;
+	if (aa_gettaskcon(pid, &aa, nullptr) != 0) {
+		return 0;
+	}
+	
+	if (aa == nullptr)
+		return 0;
+
+	if (g_strcmp0(aa, "unconfined") == 0) {
+		/* We're not going to support unconfined scopes, too hard */
+		return 0;
+	}
+
+	std::string appid(aa);
+
+	gchar * pkg = nullptr;
+	gchar * app = nullptr;
+	if (ubuntu_app_launch_app_id_parse(aa, &pkg, &app, nullptr)) {
+		appid= pkg;
+		appid += "_";
+		appid += app;
+
+		g_free(pkg);
+		g_free(app);
+	}
+	
+	return scope_checker_is_scope(checker, appid.c_str());
 }
