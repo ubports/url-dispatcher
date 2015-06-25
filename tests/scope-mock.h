9786a8c2e9ad2efc71920835b81d346311100137
Ted Gould <ted@gould.cx>
2015-06-25 16:46:52 -0500

    Make it so we don't inherit from the shared_ptr

--- tests/scope-mock.h
+++ tests/scope-mock.h
@@ -11,7 +11,7 @@ public:
 
 };
 
-class RegistryMock : public unity::scopes::RegistryProxy
+class RegistryMock : public virtual unity::scopes::Registry
 {
 	unity::scopes::testing::ScopeMetadataBuilder builder;
 	std::shared_ptr<std::map<std::string, std::exception>> scopeExceptions;
@@ -22,7 +22,8 @@ public:
 	{
 	}
 
-	unity::scopes::ScopeMetadata get_metadata(std::string const& scope_id) {
+	unity::scopes::ScopeMetadata get_metadata(std::string const& scope_id) override {
+		std::cout << "Getting Metadata" << std::endl;
 		try {
 			auto exp = (*scopeExceptions)[scope_id];
 			throw exp;
@@ -31,6 +32,17 @@ public:
 		}
 	}
 
+	/* I hate C++ so much right now */
+	std::string endpoint() override { throw new std::invalid_argument("Not implemented"); }
+	std::string identity() override { throw new std::invalid_argument("Not implemented"); }
+	std::string target_category() override { throw new std::invalid_argument("Not implemented"); }
+	std::string to_string() override { throw new std::invalid_argument("Not implemented"); }
+	int64_t timeout() override { throw new std::invalid_argument("Not implemented"); }
+	unity::scopes::MetadataMap list() override { throw new std::invalid_argument("Not implemented"); }
+	unity::scopes::MetadataMap list_if(std::function<bool(unity::scopes::ScopeMetadata const& item)>) override { throw new std::invalid_argument("Not implemented"); }
+	bool is_scope_running(std::string const&) override { throw new std::invalid_argument("Not implemented"); }
+	core::ScopedConnection set_scope_state_callback(std::string const&, std::function<void(bool is_running)>) override { throw new std::invalid_argument("Not implemented"); }
+	core::ScopedConnection set_list_update_callback(std::function<void()>) override { throw new std::invalid_argument("Not implemented"); }
 };
 
 class RuntimeMock : public RuntimeFacade
@@ -38,8 +50,12 @@ class RuntimeMock : public RuntimeFacade
 	std::shared_ptr<std::map<std::string, std::exception>> scopeExceptions;
 
 public:
+	RuntimeMock() {
+		scopeExceptions = std::make_shared<std::map<std::string, std::exception>>();
+	}
+
 	unity::scopes::RegistryProxy registry () {
-		return RegistryMock(scopeExceptions);
+		return std::make_shared<RegistryMock>(scopeExceptions);
 	}
 
 	void clearExceptions() {
