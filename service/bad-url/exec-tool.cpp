3d76545c683f6c660b2d57f2dff9914576225122
Ted Gould <ted@gould.cx>
2017-03-16 13:06:43 -0500

    Simplify the exec tool with C++ UAL API

--- service/bad-url/exec-tool.cpp
+++ service/bad-url/exec-tool.cpp
@@ -17,26 +17,10 @@
  *   Ted Gould <ted.gould@canonical.com>
  */
 
-#include <gio/gio.h>
-#include <ubuntu-app-launch.h>
+#include <ubuntu-app-launch/helper.h>
 
 int
 main (int argc, char * argv[])
 {
-	GDBusConnection * bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
-	g_return_val_if_fail(bus != NULL, -1);
-
-	gchar * exec = g_strdup_printf("qmlscene %s %s", QML_BAD_URL, g_getenv("APP_URIS"));
-
-	gboolean sended = ubuntu_app_launch_helper_set_exec(exec, NULL);
-	g_free(exec);
-
-	/* Ensuring the messages get on the bus before we quit */
-	g_dbus_connection_flush_sync(bus, NULL, NULL);
-	g_clear_object(&bus);
-
-	if (sended)
-		return 0;
-	else
-		return -1;
+	ubuntu::app_launch::Helper::setExec({"qmlscene", QML_BAD_URL});
 }
